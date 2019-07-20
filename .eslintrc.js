e6a79f40ef384bb0aa4058b313bd93528d19655d
Leandro Arzolla Coelho <leandrocoelho@DA00356.da.sa>
2019-07-20 12:05:26 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier', 'react-hooks', 'import', 'jsx-a11y'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-underscore-dangle': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'object-property-newline': [
+      'error',
+      {
+        allowAllPropertiesOnSameLine: true
+      }
+    ]
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      },
+    },
+  },
+};
